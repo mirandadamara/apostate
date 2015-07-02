d971e67be84369c0d90ce4cfc36e08b8fc4ceafa
Zebulon Young <zebulonj@gmail.com>
2015-07-02 14:09:12 -0700

    Execute actions in reaction to signal.

--- example/client/index.js
+++ example/client/index.js
@@ -1,9 +1,41 @@
+import Rx from 'rx/dist/rx.all';
+
 import cortex from './cortex';
 
-const sig1 = cortex.signal( 'sig1' );
-const sig2 = cortex.signal( 'sig2' );
+function f1( x ) {
+	return Rx.Observable.create( function( observer ) {
+		setTimeout( function() {
+			observer.onNext( ( 10 * x ) + 1 );
+			observer.onCompleted();
+		}, 5000 );
+	});
+}
+
+function f2( x ) {
+	return Rx.Observable.return( ( 10 * x ) + 2 );
+}
+
+function f3( x ) {
+	return Rx.Observable.return( ( 10 * x ) + 3 );
+}
+
+function pick( key ) {
+	return function( obj ) {
+		return Rx.Observable.return( obj[key] );
+	};
+}
+
+function echo( value ) {
+	console.log( "ECHO:", value );
+
+	return Rx.Observable.return( value );
+}
+
+const sig1 = cortex.signal( 'sig1', [pick( 'params' ), f1, f2, echo] );
+const sig2 = cortex.signal( 'sig2', [pick( 'params' ), f1, f2, f3, echo ] );
 
 console.log( "Client!" );
 
-sig1();
-sig2();
+sig1( 0 );
+sig1( 10 );
+sig2( 0 );
