0a7ce58a966690aa1358bcec55ed57a390f20bdc
Zebulon Young <zebulonj@gmail.com>
2015-07-03 00:13:37 -0700

    Allow registration of a render agent.

--- example/client/index.js
+++ example/client/index.js
@@ -1,10 +1,14 @@
 import Rx from 'rx/dist/rx.all';
 
-import cortex from './cortex';
-
 import { pick, echo } from './actions/util';
 import { f1, f2, f3 } from './actions/process';
 import increment from './actions/increment';
+
+import cortex from './cortex';
+
+cortex.express( function( state ) {
+	console.log( "FRAME:", state.toJS() );
+});
 
 const sig1 = cortex.signal( 'sig1', [pick( 'params' ), f1, f2, echo] );
 const sig2 = cortex.signal( 'sig2', [pick( 'params' ), increment, f1, increment, f2, f3, echo ] );
