96da8c2444c092ec0355dacbef7c844353e35daa
Zebulon Young <zebulonj@gmail.com>
2015-07-08 10:07:42 -0700

    Expand benchmarks.

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,13 +1,29 @@
 import Apostate from '../lib';
 
+import now from '../lib/perf/now';
+
+const timeA = now();
+const timeB = now();
+console.log( "RESOLUTION:", timeB - timeA );
+
 function action( state, input, signal ) {
 	return input;
 }
 
 const engine = Apostate();
 
-engine.signal( 'sigA', [action, action] );
+engine.signal( 'sigA', [action, action, action, action, action, action, action, action, action, action, action, action] );
 engine.signal( 'sigB', [action] );
+engine.signal( 'sigC' );
+
+engine.signals.sigA();
+engine.signals.sigA();
+engine.signals.sigA();
+engine.signals.sigA();
 
 engine.signals.sigA();
 engine.signals.sigB();
+engine.signals.sigC();
+engine.signals.sigA();
+engine.signals.sigB();
+engine.signals.sigC();
